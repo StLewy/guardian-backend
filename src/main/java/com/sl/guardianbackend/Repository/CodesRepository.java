8265038d590c1accebd54072bfdda3d5cba451bb
StLewy <52931319+StLewy@users.noreply.github.com>
2022-03-12 00:48:13 +0100

    adding code generation functionality

---
+++ src/main/java/com/sl/guardianbackend/Repository/CodesRepository.java
@@ -0,0 +1,13 @@
+package com.sl.guardianbackend.Repository;
+
+import com.sl.guardianbackend.Model.Code;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CodesRepository extends JpaRepository<Code,Long> {
+
+  Optional<Code> findByCode(String code);
+}
